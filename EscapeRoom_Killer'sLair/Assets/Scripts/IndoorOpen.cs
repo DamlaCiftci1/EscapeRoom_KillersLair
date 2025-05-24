d94846fcbee157f00bd75e8a0aeb6baff8dfe5e3
DamlaCiftci1 <dmlcftc@gmail.com>
2025-05-24 21:08:19 +0300

    Added Door Closing Animation
    We added door closing animation and changed the canvas

--- EscapeRoom_Killer'sLair/Assets/Scripts/IndoorOpen.cs
+++ EscapeRoom_Killer'sLair/Assets/Scripts/IndoorOpen.cs
@@ -2,43 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IndoorOpen : MonoBehaviour
+public class indoorOpen : MonoBehaviour
 {
     public float theDistance;
     public GameObject doorKey;
     public GameObject doorText;
-    public GameObject door;
+    public GameObject door; // Animator veya Animation component taþýyan obje
     public AudioSource doorSound;
+
+    private bool isOpen = false; // Kapý açýk mý?
 
     void Update()
     {
-        theDistance = PlayerRay.distanceFromTarget;
+        theDistance = PlayerRay.distanceFromTarget; // Oyuncuya olan mesafe
     }
 
     void OnMouseOver()
     {
-        if (theDistance <= 2)
+        if (theDistance <= 2f)
         {
             doorKey.SetActive(true);
             doorText.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                doorKey.SetActive(false);
+                doorText.SetActive(false);
+
+                if (!isOpen)
+                {
+                    door.GetComponent<Animation>().Play("Ýçe doðru kapý"); // Açýlma animasyonu
+                }
+                else
+                {
+                    door.GetComponent<Animation>().Play("DoorClose"); // Kapanma animasyonu
+                }
+
+                doorSound.Play();
+                isOpen = !isOpen; // Durumu tersine çevir
+            }
         }
         else
         {
             doorKey.SetActive(false);
             doorText.SetActive(false);
         }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (theDistance <= 2)
-            {
-                doorKey.SetActive(false);
-                doorText.SetActive(false);
-                door.GetComponent<Animation>().Play("Ýçe doðru kapý");
-                doorSound.Play();
-            }
-        }
-
     }
 
     void OnMouseExit()
